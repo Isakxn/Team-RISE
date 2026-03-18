1b5019ae3d16df683f7edd979703f5105a9548d7
Isaken <isakxm@live.no>
2026-03-18 14:59:24 +0100

    Moved model to model.js

---
+++ Teamoppgave Beckas Baguette/model.js
@@ -0,0 +1,227 @@
+const model = {
+  app: {
+    currentPage: "frontPage",
+
+    shoppingCart: {
+      cartList: [],
+      totalPrice: 0,
+    },
+  },
+
+  viewState: {
+    frontPage: {},
+
+    productPage: {
+      product: "",
+      id: Number,
+      amount: 0,
+      modifications: [],
+      totalPrice: 0,
+    },
+
+    cakePage: {
+      product: "kake",
+      id: Number,
+      modifications: [],
+      specialInstructions: "",
+      totalPrice: 0,
+    },
+
+    checkoutPage: {
+      items: [
+        {
+          id: 4,
+          name: "Grov baguette",
+          amount: 0,
+          modifications: {
+            removals: [
+              {
+                name: "uten løk",
+                price: 0,
+              },
+            ],
+            additions: [
+              {
+                name: "ekstra skinke",
+                price: 5,
+              },
+            ],
+          },
+        },
+      ],
+      totalPrice: 0,
+    },
+
+    product: {
+      kaffe: [
+        {
+          id: 1,
+          name: "Latte",
+          price: 30,
+          modifications: [
+            {
+              name: "Uten melk",
+              price: 0,
+            },
+            {
+              name: "Ekstra melk",
+              price: 5,
+            },
+            {
+              name: "Uten sukker",
+              price: 0,
+            },
+            {
+              name: "Ekstra sukker",
+              price: 5,
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: "Espresso",
+          price: 25,
+          modifications: [
+            {
+              name: "Uten melk",
+              price: 0,
+            },
+            {
+              name: "Ekstra melk",
+              price: 5,
+            },
+            {
+              name: "Uten sukker",
+              price: 0,
+            },
+            {
+              name: "Ekstra sukker",
+              price: 5,
+            },
+          ],
+        },
+        {
+          id: 3,
+          name: "Macchiato",
+          price: 35,
+          modifications: [
+            {
+              name: "Uten melk",
+              price: 0,
+            },
+            {
+              name: "Ekstra melk",
+              price: 5,
+            },
+            {
+              name: "Uten sukker",
+              price: 0,
+            },
+            {
+              name: "Ekstra sukker",
+              price: 5,
+            },
+          ],
+        },
+      ],
+      baguette: [
+        {
+          id: 4,
+          name: "Grov baguette",
+          price: 40,
+          modifications: [
+            {
+              name: "Uten løk",
+              price: 0,
+            },
+            {
+              name: "Ekstra skinke",
+              price: 5,
+            },
+            {
+              name: "Ekstra ost",
+              price: 5,
+            },
+            {
+              name: "Uten salat",
+              price: 0,
+            },
+            {
+              name: "Ekstra salat",
+              price: 5,
+            },
+          ],
+        },
+
+        {
+          id: 5,
+          name: "Fin baguette",
+          price: 45,
+          modifications: [
+            {
+              name: "Uten løk",
+              price: 0,
+            },
+            {
+              name: "Ekstra skinke",
+              price: 5,
+            },
+            {
+              name: "Ekstra ost",
+              price: 5,
+            },
+            {
+              name: "Uten salat",
+              price: 0,
+            },
+            {
+              name: "Ekstra salat",
+              price: 5,
+            },
+          ],
+        },
+        {
+          id: 6,
+          name: "Loff baguette",
+          price: 35,
+          modifications: [
+            {
+              name: "Uten løk",
+              price: 0,
+            },
+            {
+              name: "Ekstra skinke",
+              price: 5,
+            },
+            {
+              name: "Ekstra ost",
+              price: 5,
+            },
+            {
+              name: "Uten salat",
+              price: 0,
+            },
+            {
+              name: "Ekstra salat",
+              price: 5,
+            },
+          ],
+        },
+      ],
+
+      kakeStykke: [
+        { id: 7, name: "Oste Kake", price: 20 },
+        { id: 8, name: "Oreo Kake", price: 25 },
+        { id: 9, name: "Sjokolade Kake", price: 30 },
+        { id: 10, name: "Marsipan Kake", price: 10 },
+      ],
+
+      bestillingsKake: [
+        { id: 11, name: "Oste Kake", price: 200 },
+        { id: 12, name: "Oreo Kake", price: 250 },
+        { id: 13, name: "Sjokolade Kake", price: 300 },
+        { id: 14, name: "Marsipan Kake", price: 100 },
+      ],
+    },
+  },
+};
