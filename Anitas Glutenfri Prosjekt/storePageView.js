07806c06f02907a50c83727325bd88aaf666bbff
Isaken <isakxm@live.no>
2026-03-26 15:00:29 +0100

    Linked all the functions to use one parameter

--- Anitas Glutenfri Prosjekt/storePageView.js
+++ Anitas Glutenfri Prosjekt/storePageView.js
@@ -1,28 +1,25 @@
 
-    
-
-
-            
-            function storePage(){
+            function storePage(Id){
+                const storeId = model.data.store[Id];
                 const app = document.getElementById("app");   
 
                 app.innerHTML = 
                 /*HTML*/`
                         <div class="container">
                         <div class="side">
-                            <h3>${model.data.store[1].info.city}</h2>
-                            <h1>${model.data.store[1].info.storeName}</h5>
-                            <div ><img class="pictureInfo" src="${model.data.store[1].info.pictures}"></div>
+                            <h3>${storeId.info.city}</h2>
+                            <h1>${storeId.info.storeName}</h5>
+                            <div ><img class="pictureInfo" src="${storeId.info.pictures}"></div>
                                 <p>
-                                ${model.data.store[1].info.address}
+                                ${storeId.info.address}
                                 <br>
-                                telefon: ${model.data.store[1].info.phone}
+                                telefon: ${storeId.info.phone}
                                 <br>
-                                ${model.data.store[1].info.link}
+                                ${storeId.info.link}
                                 <br>
-                                ${model.data.store[1].info.email}
+                                ${storeId.info.email}
                                 <br>
-                                ${model.data.store[1].info.averageScore} ⭐⭐⭐⭐ (${model.data.store[1].info.reviewAmount}) Anmeldelser
+                                ${storeId.info.averageScore} ⭐⭐⭐⭐ (${storeId.info.reviewAmount}) Anmeldelser
                                 <br>
 
 
@@ -37,29 +34,29 @@
                         </div>
                         
                         `;
+                        showStoreReviews(Id)
                     }
-                    storePage();
-
-
-     function showStoreReviews() {
-                    const storeReviews = model.data.store[1].reviews;
-                    const storeReviewsArr = []
-
-                for (let i = 0; i < storeReviews.length; i++) {
-                    const r = storeReviews[i]
-                    storeReviewsArr.push(`
                     
-                        <div class="item">
-                        <h2>${r.name}</h2>
-                        <h5>${r.date}</h5>
-                        <p>${r.reviewText}</p>
-                        ${r.score} Stjerner.
-                        <br>
-                        (${r.likes})<button>Liker</button>
-                        </div>
                     
-                        `);
-                }
-                document.getElementById("storeReviews").innerHTML = storeReviewsArr.join('')
-                }
-                showStoreReviews();
+                    function showStoreReviews(Id) {
+                        const storeReviews = model.data.store[Id].reviews;
+                        const storeReviewsArr = []
+                        
+                        for (let i = 0; i < storeReviews.length; i++) {
+                            const r = storeReviews[i]
+                            storeReviewsArr.push(`
+                                
+                                <div class="item">
+                                <h2>${r.name}</h2>
+                                <h5>${r.date}</h5>
+                                <p>${r.reviewText}</p>
+                                ${r.score} Stjerner.
+                                <br>
+                                (${r.likes})<button>Liker</button>
+                                </div>
+                                
+                                `);
+                            }
+                            document.getElementById("storeReviews").innerHTML = storeReviewsArr.join('')
+                        }
+                        storePage(5);
