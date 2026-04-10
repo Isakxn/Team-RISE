65c22bc5c9d1cdb244cc4ab6488c4a6ef416ec0a
basementApe <71020874+basementApe@users.noreply.github.com>
2026-04-10 14:46:36 +0200

    Moved function declarations out of index.html and into a separate currentPage.js file

---
+++ Anitas Glutenfri Prosjekt/currentPage.js
@@ -0,0 +1,28 @@
+function updateView()
+{
+    html = viewCurrentPage();
+    app.innerHTML = html;
+}
+
+
+function viewCurrentPage()
+{
+    if (model.app.currentPage === "frontPage")
+    {
+        let main = "<div class='cssMain'>";
+        let cityList = addCityList();
+        let newestReviews = addNewestReviews();
+        return `
+            <div class="container">
+                ${cityList}
+                ${newestReviews}
+            </div>
+        `;
+    }
+
+    else if (model.app.currentPage === "loginPage")
+    {
+        return viewLoginPage();
+    }
+}
+
