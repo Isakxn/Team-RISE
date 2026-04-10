fe35608fe145b8b926d46ecaf32a0e1671d6f6ae
basementApe <71020874+basementApe@users.noreply.github.com>
2026-04-10 13:25:24 +0200

    Refactor: Wrapped login html in a separate view function

--- Anitas Glutenfri Prosjekt/loginPage/loginView.js
+++ Anitas Glutenfri Prosjekt/loginPage/loginView.js
@@ -1,1 +1,19 @@
 
+function viewLoginPage() 
+{
+    return `
+        <div class="containerLogin">
+            <br>
+            <div style="color: #eee;">Brukernavn:</div>
+            <input class="inputLogin" oninput="updateUsername(this.value)" onkeydown="if(event.key==='Enter'){ buttonLogin(); }">
+            <div style="color: #eee;">Passord:</div>
+            <input class="inputLogin" type="password" oninput="updatePassword(this.value)" onkeydown="if(event.key==='Enter'){ buttonLogin(); }">
+            <br>
+            <button class="buttonLogin" onclick="buttonLogin()">Logg inn</button>
+            <div class="inputLoginFail">
+                ${model.viewState.loginPage.loginInvalid}
+            </div>
+        </div>
+    `;
+}
+
