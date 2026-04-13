bc759f82b45e7b55da1f4643af04e187a3371139
basementApe <71020874+basementApe@users.noreply.github.com>
2026-04-13 12:59:57 +0200

    'Back' button for the login page, goes to front page

--- Anitas Glutenfri Prosjekt/loginPage/loginView.js
+++ Anitas Glutenfri Prosjekt/loginPage/loginView.js
@@ -3,16 +3,24 @@ function viewLoginPage()
 {
     return `
         <div class="containerLogin" style="background-image: url('/Anitas%20Glutenfri%20Prosjekt/media/cafeFolder/congAdmin.png');">
+
             <br>
             <div style="color: #feffa6;">Brukernavn:</div>
             <input class="inputLogin" oninput="updateUsername(this.value)" onkeydown="if(event.key==='Enter'){ buttonLogin(); }">
+
             <div style="color: #feffa6;">Passord:</div>
             <input class="inputLogin" type="password" oninput="updatePassword(this.value)" onkeydown="if(event.key==='Enter'){ buttonLogin(); }">
+
             <br>
             <button class="buttonLogin" onclick="buttonLogin()">Logg inn</button>
+
             <div class="inputLoginFail">
                 ${model.viewState.loginPage.loginInvalid}
             </div>
+
+            <button type="button" onclick="model.app.currentPage='frontPage'; updateView();">
+                <img src="/Anitas%20Glutenfri%20Prosjekt/media/icons/login/arrow_back_ios_new.svg" alt="" width="16" height="16" aria-hidden="true">
+            </button>
         </div>
     `;
 }
