d24658f409127982ff9c4858c32534d57b5c3b9a
basementApe <71020874+basementApe@users.noreply.github.com>
2026-04-13 13:16:08 +0200

    Update for the login page's back button: Proper styling

--- Anitas Glutenfri Prosjekt/loginPage/loginView.js
+++ Anitas Glutenfri Prosjekt/loginPage/loginView.js
@@ -4,7 +4,11 @@ function viewLoginPage()
     return `
         <div class="containerLogin" style="background-image: url('/Anitas%20Glutenfri%20Prosjekt/media/cafeFolder/congAdmin.png');">
 
-            <br>
+            <button type="button" class="loginBackButton" onclick="model.app.currentPage='frontPage'; updateView();">
+                <img src="/Anitas%20Glutenfri%20Prosjekt/media/icons/login/arrow_back_ios_new.svg" alt="" width="16" height="16" aria-hidden="true">
+           </button>
+
+           <br>
             <div style="color: #feffa6;">Brukernavn:</div>
             <input class="inputLogin" oninput="updateUsername(this.value)" onkeydown="if(event.key==='Enter'){ buttonLogin(); }">
 
@@ -18,9 +22,6 @@ function viewLoginPage()
                 ${model.viewState.loginPage.loginInvalid}
             </div>
 
-            <button type="button" onclick="model.app.currentPage='frontPage'; updateView();">
-                <img src="/Anitas%20Glutenfri%20Prosjekt/media/icons/login/arrow_back_ios_new.svg" alt="" width="16" height="16" aria-hidden="true">
-            </button>
         </div>
     `;
 }
