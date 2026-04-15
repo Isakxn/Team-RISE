67cf03b51a90f7be6f1ed8568272107e9cbc4087
basementApe <71020874+basementApe@users.noreply.github.com>
2026-04-15 11:28:19 +0200

    added log out button to loginPage that replaces the login fields and button if admin is already logged in

--- Anitas Glutenfri Prosjekt/loginPage/loginView.js
+++ Anitas Glutenfri Prosjekt/loginPage/loginView.js
@@ -2,26 +2,34 @@
 
 function viewLoginPage() 
 {
-app.innerHTML = `
+    app.innerHTML = `
         <div class="containerLogin" style="background-image: url('/Anitas%20Glutenfri%20Prosjekt/media/cafeFolder/congAdmin.png');">
 
             <button type="button" class="loginBackButton" onclick="model.app.currentPage='frontPage'; updateView();">
                 <img src="/Anitas%20Glutenfri%20Prosjekt/media/icons/login/arrow_back_ios_new.svg" alt="" width="16" height="16" aria-hidden="true">
            </button>
 
-           <br>
-            <div style="color: #feffa6;">Brukernavn:</div>
-            <input class="inputLogin" oninput="updateUsername(this.value)" onkeydown="if(event.key==='Enter'){ buttonLogin(); }">
+            ${model.app.adminLoggedIn ? 
+            `
+                <br><h2 class="loginPage_loggedIn">Logget inn som administrator</h2>
+                <br>
+                <button class="buttonLogin" onclick="buttonLogout()">Logg ut</button>
+            ` : `
+                <br>
+                <div style="color: #feffa6;">Brukernavn:</div>
+                <input class="inputLogin" oninput="updateUsername(this.value)" onkeydown="if(event.key==='Enter'){ buttonLogin(); }">
 
-            <div style="color: #feffa6;">Passord:</div>
-            <input class="inputLogin" type="password" oninput="updatePassword(this.value)" onkeydown="if(event.key==='Enter'){ buttonLogin(); }">
+                <div style="color: #feffa6;">Passord:</div>
+                <input class="inputLogin" type="password" oninput="updatePassword(this.value)" onkeydown="if(event.key==='Enter'){ buttonLogin(); }">
 
-            <br>
-            <button class="buttonLogin" onclick="buttonLogin()">Logg inn</button>
+                <br>
+                <button class="buttonLogin" onclick="buttonLogin()">Logg inn</button>
 
-            <div class="inputLoginFail">
-                ${model.viewState.loginPage.loginInvalid}
-            </div>
+                <div class="inputLoginFail">
+                    ${model.viewState.loginPage.loginInvalid}
+                </div>
+            `}
+
         </div>
     `;
 }
