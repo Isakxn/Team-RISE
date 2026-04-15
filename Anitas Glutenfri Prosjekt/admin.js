30c53697cb3a368a788f09983e1bf53e4092fc77
RemyDale <remydale@hotmail.com>
2026-04-15 14:49:58 +0200

    div Admin start

---
+++ Anitas Glutenfri Prosjekt/admin.js
@@ -0,0 +1,28 @@
+function adminControl(){
+    if(adminLoggedIn === true){
+      return {
+
+      }
+    }
+}
+
+function adminStorePage(){
+    /*html*/ `
+      <button onclick="adminDeleteRewview()"></button>`;
+    }
+    
+    <button onclick="adminDeleteStore()"></button>
+
+
+
+
+function adminDeleteRewview(){
+    if(){
+
+    }
+}
+
+
+function adminDeleteStore(){
+
+}
